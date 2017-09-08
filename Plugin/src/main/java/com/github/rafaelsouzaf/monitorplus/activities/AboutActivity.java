fb7eaebcc8ebeb79c1f88c43cd11945987861fcd
rafaelsouza <rafaelsouzaf@gmail.cm>
2017-09-07 21:27:04 -0300

    Rename some packages.

---
+++ Plugin/src/main/java/com/github/rafaelsouzaf/monitorplus/activities/AboutActivity.java
@@ -0,0 +1,16 @@
+package com.github.rafaelsouzaf.monitorplus.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.github.rafaelsouzaf.monitorplus.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+
+}
---
+++ Plugin/src/main/java/com/github/rafaelsouzaf/monitorplus/controllers/JsonDynamicallyController.java
@@ -0,0 +1,87 @@
+package com.github.rafaelsouzaf.monitorplus.controllers;
+
+import android.content.Context;
+import android.os.Handler;
+import android.util.Log;
+
+import com.github.rafaelsouzaf.monitorplus.R;
+import com.github.rafaelsouzaf.monitorplus.controllers.actions.ActionBean;
+import com.github.rafaelsouzaf.monitorplus.util.JavascriptEvaluator;
+import com.sonelli.juicessh.pluginlibrary.exceptions.ServiceNotConnectedException;
+import com.sonelli.juicessh.pluginlibrary.listeners.OnSessionExecuteListener;
+
+/**
+ * Created by rafaelsouzaf on 8/23/2017.
+ */
+
+public class JsonDynamicallyController extends BaseController {
+
+    private ActionBean action;
+
+    public JsonDynamicallyController(Context context, ActionBean action) {
+        super(context);
+        this.action = action;
+    }
+
+    @Override
+    public BaseController start() {
+
+        super.start();
+
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+
+            try {
+
+                final StringBuilder fullResponse = new StringBuilder();
+
+                getPluginClient().executeCommandOnSession(getSessionId(), getSessionKey(), action.getCommandShell(), new OnSessionExecuteListener() {
+
+                    @Override
+                    public void onCompleted(int exitCode) {
+                        System.out.println("-----exitCode-------" + exitCode);
+                        switch(exitCode){
+                            case 127:
+                                setText(getString(R.string.error));
+                                Log.d(action.getTitle(), "Tried to run a command but the command was not found on the server");
+                                break;
+                            case 0:
+                                System.out.println("========================" + fullResponse.toString());
+                                String eval = JavascriptEvaluator.evaluate(action.getJsFunctionFilter(), fullResponse.toString());
+                                setText(eval);
+                                break;
+                        }
+                    }
+                    @Override
+                    public void onOutputLine(String result) {
+                        if (fullResponse.toString().isEmpty()) {
+                            fullResponse.append(result);
+                        } else {
+                            fullResponse.append(result);
+                            fullResponse.append(System.getProperty("line.separator"));
+                        }
+                    }
+                    @Override
+                    public void onError(int error, String reason) {
+                        toast(reason);
+                    }
+                });
+            } catch (ServiceNotConnectedException e){
+                Log.d(action.getTitle(), "Tried to execute a command but could not connect to JuiceSSH plugin service");
+            }
+
+            if(isRunning()) {
+                handler.postDelayed(this, action.getTtl() * 1000L);
+            }
+
+            }
+
+        });
+
+        return this;
+
+    }
+
+}
